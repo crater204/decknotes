a865ead47c0d954983f6b9ee2f6442d4c83bde03
ONBASE\cross <chris.ross@onbase.net>
2018-05-17 10:54:29 -0400

    Init

---
+++ app/src/main/java/com/example/cross/decknotes/DeckSelector.java
@@ -0,0 +1,14 @@
+package com.example.cross.decknotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeckSelector extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck_selector);
+    }
+}
